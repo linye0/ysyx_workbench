1f789b0ca90f9510d4f169613788085623b793f4
linye0 <2629442736@qq.com>
2025-03-14 20:30:59 +0800

    NJU-ProjectN/nvboard master initialized without tracing
    NJU-ProjectN/nvboard f38aa2ac97e3d0b0ba4e71c771f004bde8d18201 Merge pull request #41 from OldLiu001/master

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,40 @@
-#include <stdio.h>
-
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+#include "verilated_vcd_c.h" //可选，如果要导出vcd则需要加上
+#include "Vour_OnOff.h"
+#include "stdio.h"
+#include <stdlib.h>
+ 
+vluint64_t main_time = 0;  //initial 仿真时间
+ 
+double sc_time_stamp()
+{
+    return main_time;
+}
+ 
+int main(int argc, char **argv)
+{
+    Verilated::commandArgs(argc, argv); 
+    Verilated::traceEverOn(true); //导出vcd波形需要加此语句
+ 
+    VerilatedVcdC* tfp = new VerilatedVcdC; //导出vcd波形需要加此语句
+ 
+    Vour_OnOff *top = new Vour_OnOff("top"); //调用VAccumulator.h里面的IO struct
+ 
+    top->trace(tfp, 0);   
+    tfp->open("wave.vcd"); //打开vcd
+ 
+    while (sc_time_stamp() < 20 && !Verilated::gotFinish()) { //控制仿真时间
+        int a = rand() & 1;
+		int b = rand() & 1;
+		top->a = a;
+		top->b = b;
+		top->eval();
+		printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+		tfp->dump(main_time); //dump wave
+        main_time++; //推动仿真时间
+    }
+    top->final();
+    tfp->close();
+    delete top;
+ 
+    return 0;
 }
