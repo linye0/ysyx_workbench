bef149651528c39ac760747f8f73e142cce05535
tracer-ysyx <tracer@ysyx.org>
2025-09-30 21:09:44 +0800

    > compile NEMU ysyx_25040131 林烨 Linux lockedcore-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:09:44 up 5 min,  1 user,  load average: 2.30, 1.57, 0.78

--- npc/vsrc/ysyx_25040131_next_pc.v
+++ npc/vsrc/ysyx_25040131_next_pc.v
@@ -18,23 +18,18 @@ end
 always @(*) begin
     if(pcImm_NEXTPC_rs1Imm == 2'b01) begin
         next_pc = pc + offset;
-        $display("[$display][TIME %t] Branch SELECTED: pcImm. PC=%h + OFFSET=%h -> NEXT_PC=%h", $time, pc, offset, next_pc);
     end
     else if(pcImm_NEXTPC_rs1Imm == 2'b10) begin
         next_pc = rs1Data + offset;
-        $display("[$display][TIME %t] Branch SELECTED: rs1Imm. RS1=%h + OFFSET=%h -> NEXT_PC=%h", $time, rs1Data, offset, next_pc);
     end
     else if(condition_branch) begin
         next_pc = pc + offset;
-        $display("[$display][TIME %t] Branch SELECTED: Conditional Branch (condition_branch=1). PC=%h + OFFSET=%h -> NEXT_PC=%h", $time, pc, offset, next_pc);
     end
     else if(pc == 32'h94) begin
         next_pc = 32'h94;
-        $display("[$display][TIME %t] Branch SELECTED: HALT at PC=%h.", $time, pc);
     end
     else begin
         next_pc = pc + 4;
-        $display("[$display][TIME %t] Branch SELECTED: Default (PC+4). PC=%h -> NEXT_PC=%h", $time, pc, next_pc);
     end
 end
 
