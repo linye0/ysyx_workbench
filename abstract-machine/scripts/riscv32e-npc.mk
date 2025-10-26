ad81292db12d514f473255854848e8a3dcc1eef8
tracer-ysyx <tracer@ysyx.org>
2025-10-26 17:05:32 +0800

    > compile NEMU ysyx_25040131 林烨 Linux lockedcore-virtual-machine 6.8.0-65-generic #68~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 15 18:06:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:05:32 up 15:47,  1 user,  load average: 0.29, 0.61, 0.87

--- abstract-machine/scripts/riscv32e-npc.mk
+++ abstract-machine/scripts/riscv32e-npc.mk
@@ -1,6 +1,8 @@
 include $(AM_HOME)/scripts/isa/riscv.mk
 include $(AM_HOME)/scripts/platform/npc.mk
-COMMON_CFLAGS += -march=rv32e_zicsr -mabi=ilp32e  # overwrite
+### 我实现的不是rv32e，改掉了
+### COMMON_CFLAGS += -march=rv32e_zicsr -mabi=ilp32e  # overwrite
+COMMON_CFLAGS += -march=rv32im_zicsr -mabi=ilp32   # overwrite
 LDFLAGS       += -melf32lriscv                    # overwrite
 
 AM_SRCS += riscv/npc/libgcc/div.S \
