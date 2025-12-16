70ec8e61a3351252fb83b1ef46fdd004879b9e20
linye0 <2629442736@qq.com>
2025-12-16 16:35:41 +0800

    ysyxsoc passed all cpu-tests!

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -1,19 +1,24 @@
 #include "include/npc.h"
 #include <am.h>
 #include <klib-macros.h>
+#include <klib.h>
 #include <riscv/ysyxsoc/include/npc.h>
 
 extern char _heap_start;
-extern char _stack_top;
-extern char _stack_bottom;
+extern char _heap_end;
+extern char _data;
+extern char _edata;
+extern char _etext;
+extern char _rodata;
+extern char _erodata;
 int main(const char *args);
 
 extern char _pmem_start;
 #define PMEM_SIZE (128 * 1024 * 1024)
 #define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
-# define npc_trap(code) asm volatile("mv a0, %0; ebreak" : :"r"(code))  
+# define npc_trap(code) asm volatile("mv a0, %0; ebreak" : :"r"(code))
 
-Area heap = RANGE(&_heap_start, &_stack_bottom);
+Area heap = RANGE(&_heap_start, &_heap_end);
 static const char mainargs[MAINARGS_MAX_LEN] = MAINARGS_PLACEHOLDER; // defined in CFLAGS
 
 void putch(char ch) {
@@ -27,6 +32,14 @@ void halt(int code) {
 }
 
 void _trm_init() {
+  // Bootloader: 将数据段从MROM复制到SRAM
+  size_t data_size = (uintptr_t)&_edata - (uintptr_t)&_data;
+  if (data_size > 0) {
+    // 数据段在MROM中的地址 = MROM起始 + .text大小 + .rodata大小
+    uintptr_t data_lma = 0x20000000 + ((uintptr_t)&_etext - 0x20000000) + ((uintptr_t)&_erodata - (uintptr_t)&_rodata);
+    memcpy((void *)&_data, (void *)data_lma, data_size);
+  }
+
   int ret = main(mainargs);
   halt(ret);
 }
--- abstract-machine/scripts/platform/ysyxsoc.mk
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -16,7 +16,7 @@ LDFLAGS   += --gc-sections -e _start
 NPCFLAGS  += -l $(shell dirname $(IMAGE).elf)/npc-log.txt
 NPCFLAGS  += -e $(IMAGE).elf -d $(NEMU_HOME)/build/riscv32-nemu-interpreter-so
 NPCFLAGS  += -m $(abspath $(IMAGE)).bin
-# NPCFLAGS  += -b
+NPCFLAGS  += -b
 
 MAINARGS_MAX_LEN = 64
 MAINARGS_PLACEHOLDER = The insert-arg rule in Makefile will insert mainargs here.
@@ -30,9 +30,9 @@ image: image-dep
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
 ## TODO: 我其实也不确定这样写对不对，之后有BUG的话记得优先回来检查一下这边
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents \
-		-O binary --only-section=.text --only-section=.rodata \
+		--only-section=.text --only-section=.rodata \
 		--only-section=.data* --only-section=.sdata* \
-		$(IMAGE).elf $(IMAGE).bin
+		-O binary $(IMAGE).elf $(IMAGE).bin
 
 ## run调用insert-arg调用image，此时IMAGE.bin里面应该已经包含了AM提供的程序运行所需的库函数
 ## elf文件是由$(AM_HOME)/Makefile生成的
