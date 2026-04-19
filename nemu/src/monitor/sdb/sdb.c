ff3832a3c06f08907235cbcf5dd184d8dd93bfdf
linye0 <2629442736@qq.com>
2026-04-19 15:24:29 +0800

    vcd bug fixed.

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -31,8 +31,8 @@ NPCState npc = {
 };
 
 VerilatedContext* contextp = NULL;
-//VerilatedVcdC* tfp = NULL;
-VerilatedFstC* tfp = NULL;
+VerilatedVcdC* tfp = NULL;
+//VerilatedFstC* tfp = NULL;
 TOP_NAME* top = NULL;
 #endif
 
